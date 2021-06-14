9599e02edb82aaa8cb0bfcacec25a7b71ba43216
kavya packt <7393253-PactkKavya@users.noreply.gitlab.com>
2021-06-14 12:38:16 +0530

    Code Bundle Updated

---
+++ Lesson 1/End/Topic A - Understanding .NET/firstPrg/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace firstPrg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shine & Rise!");
+            
+       
+        }
+    }
+}
---
+++ Lesson 1/End/Topic B - Overview of Visual Studio/WelcomeDotnetCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WelcomeDotnetCore
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string firstStatement = "Hello World!";
+            string secondStatement = "Welcome, .NET Core!";
+
+            PrintStatements(firstStatement, secondStatement);
+            
+        }
+
+        private static void PrintStatements(string s1, string s2)
+        {
+            Console.WriteLine(s1 + " " + s2);
+        }
+    }
+}
