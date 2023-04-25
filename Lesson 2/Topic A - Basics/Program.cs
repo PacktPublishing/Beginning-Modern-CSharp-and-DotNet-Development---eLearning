71a5a8e85b40fc023aac131d5b9058efa0a5fcab
Kavyashree Shah <40597017+kavyashreeshah@users.noreply.github.com>
2023-04-25 14:03:54 +0530

    Added

---
+++ Lesson 2/Topic A - Basics/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using Person;
+
+namespace Basics 
+{ 
+	class Program 
+	{ 
+		static void Main(string[] args) 
+		{ 
+			//WriteStatement(); 
+			SaySomething("Rey");
+			Female female = new Female();
+			female.name = "Cindy"; 
+			Console.WriteLine($"Her name is {female.name}");
+		} 
+		
+		// Overloading 
+		static void SaySomething() 
+		{ 
+			Console.WriteLine("I don't even know your name. Don't order me."); 
+		} 
+		static void SaySomething(string first) 
+		{ 
+			Console.WriteLine($"Cheers, {first}");
+		}
+		static void SaySomething(string first, string last) 
+		{ 
+			Console.WriteLine($"Cheers, {first} {last}"); 
+		} 
+		/* This method will write a statement * that will impress you. 
+		*/ 
+		private static void WriteStatement() 
+		{ 
+			var FirstName = "James"; 
+			var LastName = "Bond"; 
+			var FullName = FirstName + " " + LastName; 
+			Console.WriteLine("My Name is: " + FullName); 
+		}
+			
+	}
+}
+namespace Person
+{
+	public class Female
+	{
+		Public string Name { get; set; }
+	}
+}
+
